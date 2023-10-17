140ff1c2cb10c1629326f8238ce1bae2e9001f34
Leonardo Rocha <100325240+Yokiiing@users.noreply.github.com>
2023-10-16 21:56:37 -0300

    Add files via upload

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+class Aluno:
+    def __init__(self, nome, idade,email):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+
+    def perguntar (nome, idade, email):
+        print ("Que horas são?")
