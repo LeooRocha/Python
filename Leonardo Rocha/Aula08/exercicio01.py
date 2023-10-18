35b0929b418eb29ec84e8b06da22c5e2dbea8699
Leonardo Rocha <100325240+Yokiiing@users.noreply.github.com>
2023-10-18 19:10:44 -0300

    Add files via upload

---
+++ Leonardo Rocha/Aula08/exercicio01.py
@@ -0,0 +1,9 @@
+class Aluno:
+    def __init__(self, nome, idade,email):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+
+    def perguntar (nome, pergunta):
+        pergunta = input("Faça uma pergunta: ")
+        
