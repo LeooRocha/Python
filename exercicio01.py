a93cea02c289cd049d9d9a6304674dcbd1b5308e
Leonardo Rocha <100325240+Yokiiing@users.noreply.github.com>
2023-10-18 19:09:47 -0300

    Delete exercicio01.py

--- exercicio01.py
+++
@@ -1,8 +0,0 @@
-class Aluno:
-    def __init__(self, nome, idade,email):
-        self.nome = nome
-        self.idade = idade
-        self.email = email
-
-    def perguntar (nome, idade, email):
-        print ("Que horas são?")
